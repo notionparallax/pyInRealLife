3ee2e5fa3c6cbb8779c5f5f9a550b0c6ea032b1e
Ben <ben@notionparallax.co.uk>
2015-09-11 07:58:20 +1000

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,22 @@
+print "my {0} string: {1}".format("cool", "Hello there!")
+
+# madlib = " I {verb} the {object} off the {place} ".format(verb="took", object="cheese", place="table")
+
+# name = "Spongebob Squarepants"
+# print "Who lives in a Pineapple under the sea? {name!s}.".format(**locals())
+
+# point1 = {'x': 0.00000100000, 'y': 39, 'name': "Pineapple"}
+
+# print "point is at {point1!s}.".format(**locals()) #no good!
+
+# print "point is at {x}, {y} and is called {name}".format(**point1) #no good!
+
+# print "point is at {x:.2f}, {y} and is called {name}".format(**point1) #no good!
+
+# point2 = {'x': 0.00000100000, 'y': 39, 'name': "Pineapple"}
+# point3 = {'x': 0.00001100000, 'y': 29, 'name': "Orange"}
+# point4 = {'x': 0.00011100000, 'y': 19, 'name': "Pam"}
+# allPoints = [point1,point2,point3,point4]
+# for p in allPoints:
+# 	print "point is at {x:.2f}, {y:.6f} and is called {name}".format(**p)
+#read about this stuff here: https://mkaz.github.io/2012/10/10/python-string-format/
